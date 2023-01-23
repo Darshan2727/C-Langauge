ef026219339e11514bc22a54430642463c6a1b6a
darshan <darshan2701k@gmail.com>
2023-01-23 08:25:26 +0530

    first commit

---
+++ cpp/temperature.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int celsius;
+    float fahrenheit;
+    cout<<"Celsius:";
+    cin>>celsius;
+    fahrenheit=(celsius*9/5)+32;
+    cout<<"Fahrenheit:"<<fahrenheit;
+}
