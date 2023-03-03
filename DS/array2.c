56250fa6ef4377051bd2d1db5f0626e74b7ad4ea
darshan <darshan2701k@gmail.com>
2023-03-03 22:03:29 +0530

    ...

--- DS/array2.c
+++ DS/array2.c
@@ -1,29 +1,12 @@
 #include <stdio.h>
+#define n 10
+int main()
+{
+  int a[n]={10,20,30,40,50,60,70,80,90,100},i,temp;
+  temp = a[0];
+  a[0] = a[n-1];
+  a[n-1] = temp;
 
-int main(void){
-    int arr_size1;
-    int nums1[] = {10,20,30,40,50};
-    arr_size1 = sizeof(nums1)/sizeof(nums1[0]);
-    printf("Elements in original array1 are: ");  
-    pri(nums1, arr_size1);
-    int result[arr_size1];
-    for (int i = 0; i < arr_size1; i++) {
-            result[i] = nums1[i];
-             }   
-    int first = nums1[0];
-    result[0] = nums1[arr_size1 - 1];
-    result[arr_size1 - 1] = first;
-
-    printf("New array, after swapping first and last elements: ");  
-    pri(result, arr_size1);        
- }  
-  pri(int parray[], int size)
-    {
-     int i;      
-     for( i=0; i<size-1; i++)  
-      {  
-        printf("%d, ", parray[i]);  
-       } 
-    printf("%d ", parray[i]);  
-    printf("\n");   
-  } 
+  for(i=0;i<n;i++)
+    printf("%d ",a[i]);
+}
---
+++ DS/stack.c
@@ -0,0 +1,54 @@
+// stack 
+#include<stdio.h>
+#define n 5
+int a[n];
+int top = -1,i,ch;
+
+int display()
+{
+    // printf("1. pop:");
+    // printf("\n2. push:");
+    // scanf("%d",&j);
+    for(i=0;i<=top;i++)
+        printf("%d ",a[i]);
+    printf("\n");
+}
+int aa()
+{
+    printf("\n 1.push:");
+    printf("\n 2.Pop:");
+    scanf("%d",&ch);
+}
+switch(ch)
+{
+    case 1;
+    int push(int val)
+
+    {
+        if(top>=n-1)
+           printf("Array is full.....\n");
+        else{
+            ++top;
+            a[top] = val;
+         }
+    }
+    break;
+    case 2;
+    int pop()
+    {
+        if(top<0)
+            printf("\nArray is Empty..");
+        else
+            --top;
+    }
+default;
+    printf("\n wrong choice:");
+}
+
+    
+int main()
+{
+   aa();
+   display();
+
+}
